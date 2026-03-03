f6dcc5103ad29f03db60bb576498ae99ef15f32c
Amitkumar Prajapati <93379238+amitprajapa@users.noreply.github.com>
2026-03-03 23:14:28 +0530

    m

---
+++ src/com/java/practice/controlStatement/AccountBalanceCheck.java
@@ -0,0 +1,22 @@
+package com.java.practice.controlStatement;
+
+import java.util.Scanner;
+
+public class AccountBalanceCheck {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter balance");
+		
+		double curentBa= sc.nextDouble();
+		double wid = sc.nextDouble();
+		if(curentBa <=0 ){
+			System.out.println("Current Balance Can't Zero");
+		}
+		
+		if(curentBa >= wid) {
+			System.out.println("You Can withdrawal youre amount");
+		}
+	}
+}
